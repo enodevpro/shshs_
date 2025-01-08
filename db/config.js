3a852524ac67b1e5e90baadca630493dcb9110dd
unknown <qthu.developer@gmail.com>
2025-01-08 22:55:41 +0700

    e

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const { mongoose } = require("mongoose");
 
 const db = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("MONGO_URL=mongodb://localhost:27017/avatar2");
     console.log("DB Connected");
   } catch (error) {
     throw new Error(error);
